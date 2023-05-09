8a4d569801402b56b827daec9e63c4ba9e250c7e
khsabhishek <52825489+khsabhishek@users.noreply.github.com>
2023-05-09 09:03:50 +0530

    Changes in config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,45 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+// const defaultNetwork = 'fuse';
+
+// export const targetNetworkInfo: TNetworkInfo = NETWORKS.fuse;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
+  networks: {
+    fuse: {
+      url: "https://rpc.fusespark.io",
+      chainId: 123,
+      accounts: ["0bd2d6cbc72d69f03e0772ebd5366f082c6b2e7da6ad97d4272a013bc8d13061"]
+
+    }
+  },
+  // customChains: [
+  //   {
+  //     network: "fuse",
+  //     chainId: 123,
+  //     urls: {
+  //       apiURL: "https://api-goerli.etherscan.io/api",
+  //       browserURL: "https://goerli.etherscan.io"
+  //     }
+  //   }
+  // ]
+  etherscan: {
+    apiKey: {
+      fuse: "a8efec3e889147419809981047c82f5d",
+    },
+    customChains: [
+      {
+        network: "fuse",
+        chainId: 123,
+        urls: {
+          apiURL: "https://rpc.fuse.io/",
+          browserURL: "https://explorer.fusespark.io"
+        }
+      }
+    ]
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,19 +1,49 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  const FuseERC20 = await ethers.getContractFactory("FuseERC20");
+  const fuseERC20 = await FuseERC20.deploy();
+  console.log({ fuseERC20: fuseERC20.address })
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  await fuseERC20.deployed();
 
-  await lock.deployed();
+  const FuseNFT = await ethers.getContractFactory("FuseNFT");
+  const fuseNFT = await FuseNFT.deploy();
+  console.log({ fuseNFT: fuseNFT.address })
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  await fuseNFT.deployed();
+
+  const NFTAuction = await ethers.getContractFactory("NFTAuction");
+  const nftAuction = await NFTAuction.deploy(fuseERC20.address, fuseNFT.address);
+  console.log({ nftAuction: nftAuction.address })
+
+  await nftAuction.deployed();
+
+  await verifyContracts(
+    fuseERC20.address,
+    fuseNFT.address,
+    nftAuction.address
   );
+}
+
+async function verifyContracts(fuseERC20, fuseNFT, nftAuction) {
+
+  await verify(fuseERC20, "contracts/ERC20.sol:FuseERC20", []);
+  await verify(fuseNFT, "contracts/ERC721.sol:FuseNFT", []);
+  await verify(nftAuction, "contracts/marketplace.sol:NFTAuction", [fuseERC20.address, fuseNFT.address]);
+}
+
+async function verify(contractAddress: any, contract: string, args: any[]) {
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+      contract,
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
